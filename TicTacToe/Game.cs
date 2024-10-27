34856889da07ce2d8a649c6a646991febb1ea3d6
Riz Kler <rizwan.kler@gmail.com>
2024-10-27 19:53:44 +0000

    Completed code to pass the same move exceptioon.

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -10,12 +10,31 @@ namespace NoughtsAndCrosses
     {
         public int AddCounter
         { get; private set; }
+        private readonly State[] _board = new State[9];
+
+        public Game()
+        {
+            for (var i = 0; i < _board.Length; i++ )
+            {
+                _board[i] = State.Unset;
+            }
+        }
 
         public void YourMove(int index)
         {
             if (index < 1 || index > 9)
                 throw new ArgumentOutOfRangeException();
+            if (GetState(index) != State.Unset)
+                throw new InvalidOperationException();
+
+            _board[index - 1] = AddCounter % 2 == 0 ? State.Cross : State.Nought;
+           
             AddCounter++;
+        }
+
+        private State GetState(int index)
+        {
+           return _board[index - 1] ;
         }
     }
 }
