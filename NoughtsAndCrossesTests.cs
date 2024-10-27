8e87bfd35c936dfe239664559adec25855409f65
Riz Kler <rizwan.kler@gmail.com>
2024-10-27 18:19:02 +0000

    Add project files.

---
+++ NoughtsAndCrossesTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace NoughtsAndCrosses
+{
+    [TestFixture]
+    public class NoughtsAndCrossesTests
+    {
+        [Test]
+        public void StartGame_NoMoves()
+        {
+            Game game = new Game();
+            Assert.AreEqual(0, game.AddCounter);
+        }
+    }
+
+    public class Game
+    {
+        public int AddCounter
+            { get; private set; }
+    }
+}
